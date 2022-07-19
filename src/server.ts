46fd03b82c047615fde41ad44057f920579b9e33
Jarilton <jariltonpereira02@hotmail.com>
2022-07-18 22:37:40 -0300

    Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({message: "Desenvolvimento da api"})
-})
+app.use(express.json());
+
+app.use(categoriesRoutes)
 
 app.listen(3333, () => console.log("Server is running!"))
