d7d480e8611ae9ac8f16a8763edd0be00880e28e
Jarilton <jariltonpereira02@hotmail.com>
2022-07-19 00:36:52 -0300

    Principio de responsabilidade única(SRP) - Solid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();  
@@ -8,13 +9,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if(categoryAlreadyExists) {
-    return res.status(400).json({ error: "Category already exists!" });
-  }
-
-  categoriesRepository.create({ name, description })
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  
+  createCategoryService.execute({ name, description })
 
   return res.status(201).send();
 });
