6282e5774107e8cea32fea620194766b8277bbdd
Jarilton <jariltonpereira02@hotmail.com>
2022-07-18 23:02:27 -0300

    Inserindo tipagem para categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,24 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid"
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const category = {
+  const category = new Category();  
+
+  Object.assign(category, {
     name,
     description,
-    id: uuidv4(),
-  }
+    created_at: new Date(),
+  })
 
   categories.push(category);
 
-  return res.status(201).send();
+  return res.status(201).json({ category });
 });
 
 export { categoriesRoutes };
