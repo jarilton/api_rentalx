cda5206a3a65f29deecf1eced6f03c9365696ac3
Jarilton <jariltonpereira02@hotmail.com>
2022-07-19 23:07:02 -0300

    Criando repositório de especificação

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,23 @@
-class CreateSpecificationService {
-      execute() {
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
 
-      }
+interface IRequest {
+  name: string;
+  description: string;
+}
+class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name);
+
+    if(specificationAlreadyExists) {
+      throw new Error(`Specification ${name} already exists`);
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
 }
 
 export { CreateSpecificationService };
