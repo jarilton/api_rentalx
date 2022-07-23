7e15fe81b6f838566e9dac375ab2018fef978554
Jarilton <jariltonpereira02@hotmail.com>
2022-07-23 10:47:18 -0300

    Adicionando o conceito de stream

--- src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,17 @@
-
-
+import fs from "fs";
+import { parse } from "csv-parse"
 class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
+      const stream = fs.createReadStream(file.path);
 
-      execute(file: any) {
-            console.log(file);
-      };
+      const parseFile = parse();
+
+      stream.pipe(parseFile)
+
+      parseFile.on("data", async(line) => {
+            console.log(line);
+      })
+  }
 }
 
 export { ImportCategoryUseCase };
